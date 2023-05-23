8b17068ef2de3b04de39e62a1cad3b8c2e35a835
omari-elkanah <elkanahomari100@gmail.com>
2023-05-23 12:39:45 +0300

    update

--- trial and error.py
+++ trial and error.py
@@ -2,8 +2,6 @@ print("Welcome")
 name = input("What is your first name?\n")
 name2 = input("Enter your second name\n")
 no =input("Pick 1 or 2\n")
-if int(no)< 1:
-    exit()
-if int(no)> 2:
+if not int(no)== 1 or int(no)== 2:
     exit()
 print("Hey "+name + name2+" you picked "+no)
