3a2e0ccb35f72775b0555ea2dce2be358b4d582b
omari-elkanah <elkanahomari100@gmail.com>
2023-05-24 13:18:05 +0300

    updated

--- trial and error.py
+++ trial and error.py
@@ -2,6 +2,25 @@ print("Welcome")
 name = input("What is your first name?\n")
 name2 = input("Enter your second name\n")
 no =input("Pick 1 or 2\n")
-if not int(no)== 1 or int(no)== 2:
+if int(no)< 1 or int(no)> 2:
     exit()
-print("Hey "+name + name2+" you picked "+no)
+print("Hey "+name + " "+name2+" you picked "+no)
+
+stuff=["jobs"]
+stuff.append(("life"))
+stuff.extend(("vacancies","pay"))
+stuff = stuff+["cars"]
+stuff.insert(2, "umbwa")
+stuff.insert(-1, "bidet")
+print(stuff)
+
+stuff.remove("pay")
+print(stuff)
+print(stuff.pop(1))
+print(stuff)
+print("You removed "+stuff.pop(1))
+print("You removed "+stuff.pop(1))
+print("You removed "+stuff.pop(1))
+print(stuff)
+stuff.clear()
+print(stuff)
